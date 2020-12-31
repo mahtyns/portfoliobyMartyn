54f8cf3e9c45f1153a8cab523da1911046562c1c
Martyna <39212364+mahtyns@users.noreply.github.com>
2020-12-31 17:58:08 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function openList() {
+  var list = document.getElementsById("myNavMenu");
+
+
+/*
+function openHamburger() {
+    var x = document.getElementById("myNavMenu");
+    var hamb = document.getElementsByClassName("hamburger");
+  
+    if (x.className === "navMenu") {
+    x.className += "responsive";
+  } else {
+    x.className = "navMenu";
+    hamb.style.display = "none";
+
+  }
+}
+*/
